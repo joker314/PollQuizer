250c1b2f8730f06ae28dea20fa52a44a76492d05
joker314 <maxzuba22@yahoo.co.uk>
2016-06-12 11:37:19 +0100

    Starting to collect raw
    DOESN't COMPLETELY WORK YET

--- userscript.js
+++ userscript.js
@@ -1,1 +1,17 @@
+var postsNumbers = document.getElementsByClassName("roweven");
+var postsURL = [];
+var posts = [];
+for(var i = 0; i < postsNumbers.length; i++){
+  postsURL.push("https://scratch.mit.edu/discuss/posts/" + postsNumbers[i].id.substring(1) + "/source");
+}
+for(var i = 0; i < postsURL.length; i++){
+  var x = new XMLHttpRequest();
+  x.onreadystatechange = function(){
+    if(x.readyState == 4 && x.status == 200){
+      posts.push(x.responseText);
+    }
+  }
+  x.open(postsURL[i]);
+  x.send();
+}
 
