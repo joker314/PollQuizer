e0abe334506ca1406229a028a19ed8f3d7331b99
joker314 <maxzuba22@yahoo.co.uk>
2016-06-12 14:28:35 +0100

    Uh oh, AJAX is asynchronous
    This means I'll need to use a list, `ready` I'm working on it, but I think I can pass a param or something to make it synchronous if that fails

--- userscript.js
+++ userscript.js
@@ -2,16 +2,19 @@ var postsNumbers = document.getElementsByClassName("roweven");
 var postsURL = [];
 var posts = [];
 for(var i = 0; i < postsNumbers.length; i++){
-  postsURL.push("https://scratch.mit.edu/discuss/posts/" + postsNumbers[i].id.substring(1) + "/source");
+  postsURL.push("https://scratch.mit.edu/discuss/post/" + postsNumbers[i].id.substring(1) + "/source");
 }
+var x = [];
+var ready = [];
 for(var i = 0; i < postsURL.length; i++){
-  var x = new XMLHttpRequest();
+  var x.push = new XMLHttpRequest();
   x.onreadystatechange = function(){
     if(x.readyState == 4 && x.status == 200){
       posts.push(x.responseText);
     }
   }
-  x.open(postsURL[i]);
+  x.open("GET", postsURL[i]);
   x.send();
 }
+console.log(posts);
 
